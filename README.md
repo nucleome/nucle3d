1917dcbba428bc4bc8d86389c42a512649b04192
zhuxp <nimezhu@gmail.com>
2019-07-15 13:51:57 -0400

    slim

--- README.md
+++ README.md
@@ -2,32 +2,17 @@
 DNA 3D Structure
 
 ## Nucle3D Structure Text Format
+version 0.0.1
 ```
 TITLE   [NAME]        # Tab split
 GENOME  hg38          # Tab split
-[FEATURE1]  [VALUE1]  # User define features, tab split (CHR and GENOME COL[\d+] key word reserved)
-[FEATURE2]  [VALUE2]
-...
-COL0 [NAME]  [TYPE(int,string,float)] [DEFAULT VALUE IF MISSING or "NA"]   # Tab Split
-COL1 [NAME]  [TYPE] [DEFAULT VALUE IF MISSING or "NA"]
-...
-CHR chr1  250,000(or 250000)  # Tab split
+BINSIZE [binsize]
+CHR chr1 # Tab split
 i,x,y,z
-i,x,y,z,col0,col1...  # Other annotation permitted
+i,x,y,z
+..
+CHR chr2
 ..
 ```
 i is 0-index, gap of index i permitted
 
-Multi Resolution for same chromosome permitted
-
-
-## Nucle3D Structure Binary Format
-
-
-## Nucle3D Data Structure
-
-### JavaScript
-
-### GoLang
-
-## Nucle3D Query API
--- cmm2nucle3d.py
+++ cmm2nucle3d.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python
 import sys
 import xml.etree.ElementTree as ET
+
+
 def _iterChr(root):
     pass
+
+
 def main():
     if len(sys.argv) < 4:
-        print("Usage: cmm2nucle3d file.cmm genomeVersion Resolution")
+        print("Usage: cmm2nucle3d file.cmm genomeVersion binsize")
         exit(1)
     tree = ET.parse(sys.argv[1])
     genome = sys.argv[2]
-    res = sys.argv[3]
+    binsize = sys.argv[3]
     root = tree.getroot()
     lastchr = ""
+    index = 0
     print("TITLE\t%s" % root.attrib["name"])
     print("GENOME\t%s" % genome)
+    print("BINSIZE\t%s" % binsize)
     for child in root:
         tag = child.tag
         a = child.attrib
         if tag == "marker":
             if lastchr != a["chrID"]:
                 lastchr = a["chrID"]
-                print("CHR\t%s\t%s" % (lastchr,res))
-            print("%s,%s,%s,%s"  % (a["id"],a["x"],a["y"],a["z"]))
+                print("CHR\t%s" % lastchr)
+                index = 0
+            print("%s,%s,%s,%s" % (index, a["x"], a["y"], a["z"]))
+            index += 1
+
+
 if __name__ == "__main__":
     main()
